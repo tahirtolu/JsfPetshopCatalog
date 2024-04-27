9e9b48fca01c7eb864610ba26302d6ca12dc0a23
Yavuz Selim <Yavuz Selim@10.5.0.23>
2024-04-27 12:41:44 +0300

    update

---
+++ src/java/util/DBConnection.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Yavuz Selim
+ */
+public abstract class DBConnection {
+
+    private Connection connect;
+
+    public Connection getConnect() {
+        if (this.connect == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                this.connect = DriverManager.getConnection("jdbc:postgresqll://localhost:5432/petshop", "postgres", "Ys/.539374");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return connect;
+    }
+}
