23a044bfa1c805a2c0ba4d28c49a26442b6e5bcb
Yavuz Selim <Yavuz Selim@10.5.0.23>
2024-04-27 16:53:08 +0300

    update

---
+++ src/java/entity/Product.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Locale.Category;
+
+/**
+ *
+ * @author Yavuz Selim
+ */
+public class Product implements Serializable {
+
+    private long id;
+    private String isim;
+    private int fiyat;
+    private Category kategori;
+    private String marka;
+    private int stok;
+
+    public Product() {
+    }
+
+    public Product(long id, String isim, int fiyat, Category kategori, String marka, int stok) {
+        this.id = id;
+        this.isim = isim;
+        this.fiyat = fiyat;
+        this.kategori = kategori;
+        this.marka = marka;
+        this.stok = stok;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public int getFiyat() {
+        return fiyat;
+    }
+
+    public void setFiyat(int fiyat) {
+        this.fiyat = fiyat;
+    }
+
+    public Category getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(Category kategori) {
+        this.kategori = kategori;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+}
